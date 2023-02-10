54ee4136e701c595a39ca9a9d375e9f365ad151e
mnqood <mnasr@nqoodlet.com>
2023-02-10 18:35:26 +0200

    init

---
+++ database/migrations/2023_02_08_150424_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('from');
+            $table->string('to');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('trip_types');
+            $table->unsignedBigInteger('bus_id');
+            $table->foreign('bus_id')->references('id')->on('buses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+};
